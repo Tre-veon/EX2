796e34b55e02c00b7aa66f7f639c628ad64f1dc4
Tre-veon <120367@swcsd.us>
2023-01-24 14:17:18 -0500

    tre

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        //Treveon Aponte
+        //Lab 2
         String[] days = {"Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
         int[] tempHigh = {45, 29, 10, 22, 41, 28,33};
         int[] precipitation = { 95, 60, 25, 5, 0 , 75 ,90};
