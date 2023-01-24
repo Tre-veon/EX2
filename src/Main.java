8ea0fb1378ba2db0f457f5fd203518d31bc5d118
Tre-veon <120367@swcsd.us>
2023-01-24 14:16:05 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] days = {"Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+        int[] tempHigh = {45, 29, 10, 22, 41, 28,33};
+        int[] precipitation = { 95, 60, 25, 5, 0 , 75 ,90};
+
+
+        for (int i = 0; i < days.length; i++) {
+            if (tempHigh[i] <=32&&precipitation[i] >50) {
+                System.out.println(days[i]);
+            }
+
+        }
+    }
+}
